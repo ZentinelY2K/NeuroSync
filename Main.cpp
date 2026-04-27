8550ece497943e180f234eb93152dcdaf6e5d187
ZentinelY2K <axetl14@gmail.com>
2026-04-27 07:57:00 -0600

    2 Channel Update

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
-int electrode = 35;
+int electrode1 = 35;
+int electrode2 = 34;
+int buzzer = 17;
 void setup(){
     Serial.begin(115200);
-    pinMode(electrode,INPUT);
+    pinMode(electrode1,INPUT);
+    pinMode(electrode2,INPUT);
+    pinMode(buzzer,OUTPUT);
     
 }
-
+   
 void loop(){
-    float readings = analogRead(electrode);
-    Serial.println(readings);
-    delay(200);
+    float readings1 = analogRead(electrode1);
+    float readings2 = analogRead(electrode2);
+    Serial.println(readings1);
+    Serial.println(" ");
+    Serial.println(readings2);
+
+    if (readings1>=2100 && readings2 >= 2150){
+      digitalWrite(buzzer,HIGH);
+      
+    }
+
+    else{
+      digitalWrite(buzzer,LOW);
+    }
+   
 }
